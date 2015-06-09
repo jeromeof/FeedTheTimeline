ae3ee7ce4241103a25bd10acdb4287433aca78b9
jeromeof <jerome.oflaherty@gmail.com>
2015-06-09 12:07:16 +0100

    Change the JS code to be IOS compatibile

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,70 +1,78 @@
 Pebble.addEventListener("ready", function(e) {
     console.log("Ready");
-})
+});
 
 Pebble.addEventListener('showConfiguration', function(e) {
     console.log("config");
-  	Pebble.getTimelineToken(
-	  function (token) {
-  		  var URL = 'http://feedthetimeline.appspot.com/config?token=' + token;
-		  console.log('Configuration window opened. ' + URL);
-		  Pebble.openURL(URL);
-	  },
-	  function (error) { 
-    	console.log('Error getting timeline token: ' + error);
-	  }
+      Pebble.getTimelineToken(
+       function (token) {
+        var URL = 'http://feedthetimeline.appspot.com/config?token=' + token;
+        console.log('Configuration window opened. ' + URL);
+        Pebble.openURL(URL);
+     },
+       function (error) { 
+          console.log('Error getting timeline token: ' + error);
+     }
 	);
 
 });
 
+function timelineSubscribe(subscription) {
+      Pebble.timelineSubscribe(subscription,
+          function () { 
+              console.log('Subscribed from ' + subscription);
+          }, 
+          function (errorString) { 
+               console.log('Error subscribing for topic: ' + subscription + ' Error:'+ errorString);
+          }
+       );
+}
+
+function timelineUnsubscribe(topic) {
+        Pebble.timelineUnsubscribe(topic,
+            function () { 
+               console.log('Unsubscribed from ' + topic);
+            },
+            function (errorString) { 
+               console.log('Error unsubscribing from topic: ' + topic + ' Error:' + errorString);
+            }
+         );
+}
+
+
 Pebble.addEventListener("webviewclosed", function(e) {
     console.log("configuration closed");
-    if (e.response != '') {
-		var configuration = JSON.parse(decodeURIComponent(e.response));
+    if (e.response !== '') {
+       var configuration = JSON.parse(decodeURIComponent(e.response));
 		console.log('subscriptions' + configuration.subscriptions);
 		var subscriptions = configuration.subscriptions;
 		
 		// First get our current subscriptions
 		Pebble.timelineSubscriptions(
-	    	function (topics) {
-	    	
-	    		// First go through current topics and see if we need to remove any
-	    		for (var t = 0; t < topics.length; t++) {
-	    			var topic = topics[t];
-	    			// If our topic isn't found then unsubscribe from topic
-	    			if (subscriptions.indexOf(topic) == -1) {
-	 		    		Pebble.timelineUnsubscribe(topic,
-				    		function () { 
-  								console.log('Unsubscribed from ' + topic);
-							}, 
-				    		function (errorString) { 
-					    		console.log('Error unsubscribing from topic: ' + topic + ' Error:' + errorString);
-			  				}
-						);
+       function (topics) {
+
+        // First go through current topics and see if we need to remove any
+         for (var t = 0; t < topics.length; t++) {
+            var topic = '' + topics[t];
+            // If our topic isn't found then unsubscribe from topic
+            if (subscriptions.indexOf(topic) == -1) {
+              timelineUnsubscribe(topic);
 					}
 				}
 				
 				for (var s = 0; s < subscriptions.length; s++) {
 					var subscription = subscriptions[s];
-	    			// If our subscription isn't found then subscribe to him
-	    			if (topics.indexOf(subscription) == -1) {
-              
-	 		    		Pebble.timelineSubscribe(subscription,
-				    		function () { 
-  								console.log('Subscribed from ' + subscription);
-							}, 
-				    		function (errorString) { 
-			                  console.log('Error subscribing for topic: ' + subscription + ' Error:'+ errorString);
-			  				}
-						);
-	    			}
+            // If our subscription isn't found then subscribe to him
+           if (topics.indexOf(subscription) == -1) {
+              timelineSubscribe(subscription);
+           }
 				}
-		    }, 
-  			function (errorString) { 
-			    console.log('Error retrieving Subscriptions? '+ errorString);
-		  }
+       }, 
+        function (errorString) { 
+            console.log('Error retrieving Subscriptions? '+ errorString);
+        }
 		);
     } else {
-		console.log("no options received");
+       console.log("no options received");
     }
 });
