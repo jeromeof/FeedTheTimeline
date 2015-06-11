37ac0a5db7e2d2475900f5949a348182e6bdb95d
jeromeof <jerome.oflaherty@gmail.com>
2015-06-11 19:53:28 +0100

    Another attempt at fixing IOS config

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,20 +1,3 @@
-Pebble.addEventListener("ready", function(e) {
-    console.log("Ready");
-    Pebble.addEventListener('showConfiguration', function(e) {
-      console.log("config");
-        Pebble.getTimelineToken(
-          function (token) {
-            var URL = 'http://feedthetimeline.appspot.com/config?token=' + token;
-            console.log('Configuration window opened. ' + URL);
-            Pebble.openURL(URL);
-          },
-          function (error) { 
-             console.log('Error getting timeline token: ' + error);
-          }
-      );
-
-   });
-});
 
 
 
@@ -40,40 +23,56 @@ function timelineUnsubscribe(topic) {
          );
 }
 
+Pebble.addEventListener("ready", function(e) {
+    console.log("Ready");
+    Pebble.addEventListener('showConfiguration', function(e) {
+      console.log("config");
+        Pebble.getTimelineToken(
+          function (token) {
+            var URL = 'http://feedthetimeline.appspot.com/config?token=' + token;
+            console.log('Configuration window opened. ' + URL);
+            Pebble.openURL(URL);
+          },
+          function (error) { 
+             console.log('Error getting timeline token: ' + error);
+          }
+      );
 
-Pebble.addEventListener("webviewclosed", function(e) {
-    console.log("configuration closed");
-    if (e.response !== '') {
-       var configuration = JSON.parse(decodeURIComponent(e.response));
-		console.log('subscriptions' + configuration.subscriptions);
-		var subscriptions = configuration.subscriptions;
-		
-		// First get our current subscriptions
-		Pebble.timelineSubscriptions(
-       function (topics) {
+   });
+    Pebble.addEventListener("webviewclosed", function(e) {
+      console.log("configuration closed");
+      if (e.response !== '') {
+         var configuration = JSON.parse(decodeURIComponent(e.response));
+        console.log('subscriptions' + configuration.subscriptions);
+        var subscriptions = configuration.subscriptions;
+        
+        // First get our current subscriptions
+        Pebble.timelineSubscriptions(
+            function (topics) {
+  
+            // First go through current topics and see if we need to remove any
+             for (var t = 0; t < topics.length; t++) {
+                var topic = '' + topics[t];
+                // If our topic isn't found then unsubscribe from topic
+                if (subscriptions.indexOf(topic) == -1) {
+                  timelineUnsubscribe(topic);
+                }
+            }
 
-        // First go through current topics and see if we need to remove any
-         for (var t = 0; t < topics.length; t++) {
-            var topic = '' + topics[t];
-            // If our topic isn't found then unsubscribe from topic
-            if (subscriptions.indexOf(topic) == -1) {
-              timelineUnsubscribe(topic);
-					}
-				}
-				
-				for (var s = 0; s < subscriptions.length; s++) {
-					var subscription = subscriptions[s];
-            // If our subscription isn't found then subscribe to him
-           if (topics.indexOf(subscription) == -1) {
-              timelineSubscribe(subscription);
-           }
-				}
-       }, 
-        function (errorString) { 
-            console.log('Error retrieving Subscriptions? '+ errorString);
-        }
-		);
-    } else {
-       console.log("no options received");
-    }
+            for (var s = 0; s < subscriptions.length; s++) {
+                var subscription = subscriptions[s];
+                // If our subscription isn't found then subscribe to him
+               if (topics.indexOf(subscription) == -1) {
+                  timelineSubscribe(subscription);
+               }
+            }
+           }, 
+            function (errorString) { 
+                console.log('Error retrieving Subscriptions? '+ errorString);
+             }
+          );
+      } else {
+         console.log("no options received");
+      }
+   });
 });
